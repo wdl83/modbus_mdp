c02dba95225dc5ea5cb5405bd366d439d51a40af
Wlodzimierz Lipert <wlodzimierz.lipert@gmail.com>
2020-09-27 20:49:00 +0200

    Bugfix - getopt() return type is int. Removed leftovers from libmodbus use.

--- Ensure.h
+++ Ensure.h
@@ -1,14 +1,3 @@
 #pragma once
 
 #include "mdp/Ensure.h"
-
-#include <modbus/modbus.h>
-
-struct ReportModbusErrNo : public std::runtime_error
-{
-    ReportModbusErrNo(const std::string &msg):
-        std::runtime_error{std::string{::modbus_strerror(errno)} + " at " + msg}
-    {}
-};
-
-using ModbusRuntimeError = Exception<ReportModbusErrNo>;
